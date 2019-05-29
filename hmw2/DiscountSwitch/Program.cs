09202e520a6868adb4edc39245390dfad32bb903
LamberJack <vladmartunovskiy@gmail.com>
2019-05-29 11:59:59 +0300

    hmw2save3

--- hmw2/DiscountSwitch/Program.cs
+++ hmw2/DiscountSwitch/Program.cs
@@ -10,30 +10,31 @@ namespace homework_all
     {
         static void Main()
         {
-            Console.WriteLine("Нипиши на сколько денег ты кипил товаров?");
+            Console.WriteLine("Нипиши на сколько денег ты купил товаров?");
 			decimal money = Convert.ToDecimal(Console.ReadLine());
             decimal discount;
 
-            switch (true)
+            switch (money)
             {
-            case (money<0m): 
-            Console.WriteLine("вам не хватет денег купить что-то, идите работать");
-            break;
-            case (money<300m):
-            Console.WriteLine("цена вашей покупки ${0:#.##}, ваша скидка составит $0.000", money);
-            break;
-            case (money<400m): 
-            discount=0.03m;
-            Console.WriteLine("цена вашей покупки ${0:#.##}, ваша скидка составит ${1:#.##}", money-=money*discount, money*discount);
-            break;
-            case (money<500m): 
-            discount=0.05m;
-            Console.WriteLine("цена вашей покупки ${0:#.##}, ваша скидка составит ${1:#.##}",  money-=money*discount, money*discount);
-            break;
-            default:
-            discount=0.07m;
-            Console.WriteLine("цена вашей покупки ${0:#.##}, ваша скидка составит ${1:#.##}",  money-=money*discount, money*discount);
-            break;
+                case decimal check when (check<0m): 
+                    Console.WriteLine("вам не хватет денег купить что-то, идите работать");
+                    break;
+                case decimal check when (check<300m):
+                    discount=0.00m;
+                    Console.WriteLine("цена вашей покупки ${0:#.##}, ваша скидка составит ${1}", money, discount);
+                    break;
+                case decimal check when (check<400m): 
+                    discount=0.03m;
+                    Console.WriteLine("цена вашей покупки ${0:#.##}, ваша скидка составит ${1:#.##}", money-=money*discount, money*discount);
+                    break;
+                case decimal check when (check<500m): 
+                    discount=0.05m;
+                    Console.WriteLine("цена вашей покупки ${0:#.##}, ваша скидка составит ${1:#.##}",  money-=money*discount, money*discount);
+                    break;
+                default:
+                    discount=0.07m;
+                    Console.WriteLine("цена вашей покупки ${0:#.##}, ваша скидка составит ${1:#.##}",  money-=money*discount, money*discount);
+                    break;
         	}
         	Console.ReadKey();
         }
