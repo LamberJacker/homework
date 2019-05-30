6ac506fec2ca121fa02a77bb2967ea44aec1fe7e
LamberJack <vladmartunovskiy@gmail.com>
2019-05-31 00:29:48 +0300

    hmw3save5

--- hmw3/threeArray/Program.cs
+++ hmw3/threeArray/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace homework_all
 {
@@ -15,58 +11,58 @@ namespace homework_all
             short[] arrShort = new short [10];
             int[] arrInt = new int [10];
 
-            for (int i = 0; i < 10; i++) //Инициализируем массивы значениями "-1"
-            {
-                a[i] = b[i] = c[i] = -1;
-            }
-
             bool flagByte = false; //флаг для заполнения массива типа byte
             bool flagShort = false; //флаг для заполнения массива типа short
             bool flagInt = false; //флаг для заполнения массива типа int
 
-            Random rnd = new Random(0, 9223372036854775807); //Создадим экземпляр класса Random
+            Random rnd = new Random(); //Создадим экземпляр класса Random
             int rndValue = 0; //число которое рандомно генерируеться
 
             int i = 0; //счетчик для цыкла byte
             int j = 0; //счетчик для цыкла short
             int k = 0; //счетчик для цыкла Int
 
-            while(flagByte == false & flagShort== false & flagInt == false)
+            while(flagByte == false | flagShort == false | flagInt == false)
             {
-                rndValue=rnd.Next();
+                rndValue=rnd.Next(1000000);
                 Console.WriteLine("Проверка самого себя рандомное число = "+ rndValue);
-                switch(rndValuse)
+                
+                if (rndValue <= 255 & i < 10)
                 {
-                case int check when (rndValue<128):
-                        {
-                        if(rndValue == arrByte[10])
-                        {
-                        flagByte == true;
-                        goto case 2; 
-                        }                       
-                        arrByte[i]=rndValue;
-                        i++;
-                        }
-                case int check when (rndValue<32768):
-                        {
-                        if(rndValue == arrShort[10])
-                        {
-                        flagShort == true;
-                        goto case 3; 
-                        }                     
-                        arrShort[j]=rndValue;
-                        j++;
-                        }
-                default:
-                        {
-                        if(rndValue == arrShort[10]);                       
-                        arrInt[k]=rndValue;
-                        k++;
-                        }
-
-
+                arrByte[i] = (byte) rndValue;
+                i++;
+                if (i == 10) flagByte = true;
+                }
+                else if (rndValue <= 32767 & j < 10)
+                {
+                arrShort[j] = (short) rndValue;
+                j++;
+                if (j == 10) flagShort = true;
+                } else if (rndValue <= 2147483647 & k < 10)
+                {
+                arrInt[k] = rndValue;
+                k++;
+                if (k == 10) flagInt = true;
                 }
             }
+
+            // Выводим на экран:
+            Console.WriteLine(" ");
+            for (int m = 0; m < 10; m++)
+			{
+                Console.WriteLine("Byte ["+ m + "]" + " = " + arrByte[m]);
+			}
+            Console.WriteLine(" ");
+            for (int m = 0; m < 10; m++)
+			{
+                Console.WriteLine("Short ["+ m + "]" + " = " + arrShort[m]);
+			}
+            Console.WriteLine(" ");
+            for (int m = 0; m < 10; m++)
+			{
+                Console.WriteLine("Int ["+ m + "]" + " = " + arrInt[m]);
+			}
+            Console.ReadKey();
         }
     }
 }
