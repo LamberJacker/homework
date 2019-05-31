e81b9ee3e7e57b1b6ac4291ad1516472ce6afb78
LamberJack <vladmartunovskiy@gmail.com>
2019-05-31 12:49:17 +0300

    hmr3save7

---
+++ hmw3/bubleSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace homework_all
+{
+    class arrSort
+    {
+        static void Main()
+        {
+            //Исходные даные
+            int[] arr = new int [10]; //Длина массива
+            Random rnd = new Random(); //Создадим экземпляр класса Random
+            int rndValue = 0; //число которое рандомно генерируеться
+            
+            //Инициализируем массив случайными числами
+            for (int i = 0; i < arr.Length; i++)
+            {
+                rndValue=rnd.Next(100);
+                arr[i] = rndValue;
+            }
+
+            // Выводим на экран изначатьный:
+            Console.WriteLine("Данно массив: ");
+            for (int i = 0; i < arr.Length; i++)
+			{
+                Console.Write(arr[i]+"\t");
+			}
+
+            //Сортировка по возростанию
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = 0; i < arr.Length-1-j; j++)
+                {
+                    if(arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            // Выводим на экран:
+            Console.WriteLine("\n");
+            Console.WriteLine("Получим массив: ");
+            for (int i = 0; i < arr.Length; i++)
+			{
+                Console.Write(arr[i]+"\t");
+			}
+            Console.ReadKey();
+        }
+    }
+}
---
+++ hmw3/gameDog/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace homework_all
+{
+    class GameDog
+    {
+        static void Main()
+        {
+            //Исходные даные
+            bool flagCheck = false;
+            char[] arr = {'@', '_', '_', '_', '_', '_', '_', '_', '_', '_',};
+            Console.WriteLine("Мы играем в игру \"Собачка\". \nДоберись до конца что бы выиграть! \nТвой путь:" );
+            for (int i = 0; i < arr.Length; i++)
+			{
+                Console.Write(arr[i]+" ");
+			}
+            do
+            {
+                Console.Write("\n\nP.S. Нажимай \"a\" или \"d\" что бы двигать собачку влево вправо соответсвенно" );
+                turn = Convert.ToChar(Console.Read()); //ход игрока
+                
+                if (arr[9] == '@') break;   
+                    
+                    switch(turn)
+                    {
+                        case 'a':
+                        for (int i = 0; i < arr.Length; i++)
+		            	{
+
+		            	}
+
+                        break;
+                        case 'd':
+
+                        break;
+                        default:
+                        Console.WriteLine("Ты не правильно управляешь собакой нажимай \"a\" или \"d\"" );
+                        break;
+                    }
+            }while(chek == true);
+            Console.ReadKey();
+        }
+    }
+}
