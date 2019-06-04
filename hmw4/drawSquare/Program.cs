97badbc3f785426f02fcff42ca3774d95de186da
LamberJacker <50924124+LamberJacker@users.noreply.github.com>
2019-06-04 09:24:20 +0300

    hmw4save5

--- hmw4/drawSquare/Program.cs
+++ hmw4/drawSquare/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+    
+using System;
 
 namespace homework_all
 {
@@ -8,76 +9,92 @@ namespace homework_all
         {
             //Исходные даные
             Console.WriteLine("Введите на сколько рядков и столбцов отрисовать квадрат");
-            Console.WriteLine("Введи количество рядков и столбцов (непарное число) : ");
-            int rowcol = Convert.ToInt32(Console.ReadLine());
-            bool check = true;
-
-            if (rowcol%2 == 0)
+            Console.WriteLine("Введи количество рядков и столбцов (непарное натуральное число до 100): ");
+            Console.WriteLine("Думаю такого квадрата тебе хватит");
+            
+            string userValue = Console.ReadLine();
+            int rowcol = 0; //переменная количества строк и колоннок
+            bool canConvert = Int32.TryParse(userValue, out rowcol); //проба конвертировать число
+            
+            if (canConvert == false) //Проверка значения числа на число с плавующей точкой и ввод букв
             {
-                check = false;
-            } 
+                Console.WriteLine("Ты мне наврал и ввел плохое число!");
+                Console.ReadKey();
+            }
+            else
+            {
+                bool check = true;
+                    
+                if (rowcol%2 == 0 || rowcol >= 100 || rowcol < 0) //Проверка значения числа на парность, не более 100, менее нуля
+                {
+                    check = false;
+                    Console.WriteLine("Ты мне наврал и ввел плохое число!");
+                    Console.ReadKey();
+                    return;
+                }
+
+                char[][] arr = new char[rowcol][];
                 
-            char[][] arr = new char[rowcol][];
-
-            //Логика:
-            if (check == true)
-            {
-                for (int i = 0; i < arr.Length; i++)
+                //Логика:
+                if (check == true)
                 {
-                    char[] temp = new char[rowcol];
-                    for (int j = 0; j < temp.Length; j++)
+                    for (int i = 0; i < arr.Length; i++)
                     {
-                        if (i == 0 || j == 0 || i == arr.Length-1 || j == temp.Length-1) 
+                        char[] temp = new char[rowcol];
+                        for (int j = 0; j < temp.Length; j++)
                         {
-                            temp[j] = '*';
+                            if (i == 0 || j == 0 || i == arr.Length-1 || j == temp.Length-1) 
+                            {
+                                temp[j] = '*';
+                            }
+                            else if (i == arr.Length/2 & j == arr.Length/2)
+                            {
+                                temp[j] = '*';
+                            }
+                            else if (i == arr.Length/2-1 & j == arr.Length/2)
+                            {
+                                temp[j] = '*';
+                            }
+                            else if (i == arr.Length/2+1 & j == arr.Length/2)
+                            {
+                                temp[j] = '*';
+                            }
+                            else if (i == arr.Length/2 & j == arr.Length/2+1)
+                            {
+                                temp[j] = '*';
+                            }
+                            else if (i == arr.Length/2 & j == arr.Length/2-1)
+                            {
+                                temp[j] = '*';
+                            }
+                            else
+                            {
+                                temp[j] = ' ';
+                            }
                         }
-                        else if (i == arr.Length/2 & j == arr.Length/2)
-                        {
-                            temp[j] = '*';
-                        }
-                        else if (i == arr.Length/2-1 & j == arr.Length/2)
-                        {
-                            temp[j] = '*';
-                        }
-                        else if (i == arr.Length/2+1 & j == arr.Length/2)
-                        {
-                            temp[j] = '*';
-                        }
-                        else if (i == arr.Length/2 & j == arr.Length/2+1)
-                        {
-                            temp[j] = '*';
-                        }
-                        else if (i == arr.Length/2 & j == arr.Length/2-1)
-                        {
-                            temp[j] = '*';
-                        }
-                        else
-                        {
-                            temp[j] = ' ';
-                        }
+                        arr[i] = temp;
                     }
-                    arr[i] = temp;
                 }
-            }
 
-            //Выводим на экран:
-            if (check == true) 
-            {
-                Console.WriteLine("\n");
-                for (int i = 0; i < arr.Length; i++)
+                //Выводим на экран:
+                if (check == true) 
                 {
-                    for (int j = 0; j < arr[i].Length; j++)
+                    Console.WriteLine("\n");
+                    for (int i = 0; i < arr.Length; i++)
                     {
-                        Console.Write(arr[i][j]);
+                        for (int j = 0; j < arr[i].Length; j++)
+                        {
+                            Console.Write(arr[i][j]);
+                        }
+                        Console.WriteLine();
                     }
-                    Console.WriteLine();
                 }
+                else 
+                {
+                    Console.WriteLine("Ты мне наврал и ввел плохое число!");
+                }
+                Console.ReadKey();
             }
-            else 
-            {
-                Console.WriteLine("Ты мне наврал и ввел неправильное число!");
-            }
-            Console.ReadKey();
         }
     }
 }
