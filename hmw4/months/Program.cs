e9dc7feeff24561738821c4411c240c08c3a3666
LamberJacker <50924124+LamberJacker@users.noreply.github.com>
2019-06-04 09:25:16 +0300

    hmw4save1

--- hmw4/months/Program.cs
+++ hmw4/months/Program.cs
@@ -7,24 +7,24 @@ namespace homework_all
         static void Main()
         {
             //Исходные даные
-            int[] arr = new int [12];
-            
+            int[] arr = new int[12];
+
             //Заполнение массива
             for (int i = 0; i < arr.Length; i++)
             {
-                if (i == 1) 
-                    {
-                        arr[i] = 28;
-                        continue;
-                    } 
-                if (i == 7) 
-                    {
-                        arr[i] = 31;
-                        continue;
-                    }
-                if (i%2 == 0 & i<7 ) arr[i] = 31;
+                if (i == 1)
+                {
+                    arr[i] = 28;
+                    continue;
+                }
+                if (i == 7)
+                {
+                    arr[i] = 31;
+                    continue;
+                }
+                if (i % 2 == 0 & i < 7) arr[i] = 31;
                 else if (i < 7) arr[i] = 30;
-                else if (i%2 == 0 & i > 7) arr[i] = 30;
+                else if (i % 2 == 0 & i > 7) arr[i] = 30;
                 else arr[i] = 31;
             }
 
@@ -32,7 +32,7 @@ namespace homework_all
             Console.WriteLine("Дней в месяцах года: ");
             for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine("В " + (i+1) + " месяце " + arr[i]+" дней ");
+                Console.WriteLine("В " + (i + 1) + " месяце " + arr[i] + " дней ");
             }
             Console.ReadKey();
         }
