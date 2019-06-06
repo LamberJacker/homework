c74723aa48f4ac2d179e5b21210ec7735c33c021
vladyslav.martynovskyi <vladyslav.martynovskyi@gmail.com>
2019-06-06 23:23:43 +0300

    HMW5 task 1 and 2 well done

--- hmw5/stringArray/Program.cs
+++ hmw5/stringArray/Program.cs
@@ -6,76 +6,33 @@ namespace VladyslavMartynovskyi
     {
         static void Main()
         {
-        	string userImput;
-        	bool flag0, flag1, flag2, flag3, flag4, flag5, flag6, flag7, flag8, flag9, flag10, flag11, flag12, flag13, flag14, flag15;
-            do
-            {
-            	Console.WriteLine("Please, write your full name in one row, only like <lastname> <name> <patronymic>:");
-	            
-	            userImput = Console.ReadLine(); // cчитуем строку пользователя
-	            
-	            // снижаем ризк неправильного ввода
-	            flag0 = userImput.Contains("0");
-	            flag1 = userImput.Contains("1");
-	            flag2 = userImput.Contains("2");
-	            flag3 = userImput.Contains("3");
-	            flag4 = userImput.Contains("4");
-	            flag5 = userImput.Contains("5");
-	            flag6 = userImput.Contains("6");
-	            flag7 = userImput.Contains("7");
-	            flag8 = userImput.Contains("8");
-	            flag9 = userImput.Contains("9");
-	            flag10 = userImput.Contains("@");
-	            flag11 = userImput.Contains(".");
-	            flag12 = userImput.Contains(",");
-	            flag13 = userImput.Contains("-");
-	            flag14 = userImput.Contains("!");
-	            flag15 = userImput.Contains("/");
-
-	        } while (flag0 || flag1 || flag2 || flag3 || flag4 || flag5 || flag6 || flag7 || flag8 || flag9 || flag10 || flag11 || flag12 || flag13 || flag14 || flag15);
-
-           	while (userImput.Contains("  ")) //убираем лишние пробелы
-           	{ 
-           		userImput = userImput.Replace("  ", " ");
-           	}
-
-           	string[] wordsSplit = userImput.Trim().Split(' ');//разбиваем массив по пробелам
-           	
-           	// Логика программы:
-            for (int i = 0; i < wordsSplit.Length; i++)
-            {
-            	string tempWord = ""; // дополнительная переменая для записи слова из чаров
-            	for(int j = 0; j < wordsSplit[i].Length; j++)
-        		{
-        			if (j == 0)
-        			{	
-        				string firstLetter = ""; // string переменая для записи большой первой буквы
-        				firstLetter = wordsSplit[i][j].ToString().ToUpper();
-        				tempWord += firstLetter;
-        			}
-        			else
-        			{	
-        				tempWord += wordsSplit[i][j];
-        			}
-    			}
-  				
-  				if(i == 0) 
-  				{	
-					wordsSplit[i] = tempWord; //записуем первое слово полностью
-  				}
-  				else 
-  				{	
-  					wordsSplit[i] = wordsSplit[i].Substring(0,1).ToUpper() + "."; //записуем первую букву в большом регистре
-  				}
-  						
-            }
-
-            // Выводим данные
-			for (int i = 0; i < wordsSplit.Length; i++)
-			{	
-				Console.Write(wordsSplit[i]+" ");
+            Random rnd = new Random(); // ссылка на метод рандом
+            int words = rnd.Next(5, 10); // рандомное значения количества слов в массиве 
+            int letters = rnd.Next(3, 8); // количество букв
+        	string[] arrString = new string[words]; // создаем массив
+            byte counterLetter = 0; // счетчик для согласных букв
+        	string consonants = "bcdfghjklmnpqrstvwxyz"; // перечисление согласных
+            
+            // Инициализируем массив слов:
+            for (int i = 0; i < words; i++)
+			{   
+                for (int j = 0; j < letters; j++)
+			    {
+                    char someChar = (char) rnd.Next(97, 122);
+                    for (int k = 0; k < 21; k++)
+			        {
+                        if (someChar == consonants[k]) counterLetter++;
+			        }
+                    arrString[i] += someChar.ToString();
+			    }
 			}
-			
+
+            // Вывод на экран:
+            for (int i = 0; i < words; i++)
+            {
+                Console.Write(arrString[i]+" ");
+            }
+            Console.WriteLine("\nВ данном предложении количество согласных букв равно : " + counterLetter + " шт.");
             Console.ReadKey();      
         }
     }
